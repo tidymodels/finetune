081939a91ecd8d5d658dcae2f66a967616de860e
Max Kuhn <mxkuhn@gmail.com>
2020-07-11 11:22:56 -0400

    plot for the racing values

---
+++ R/plot_race.R
@@ -0,0 +1,20 @@
+#' Plot racing results
+#'
+#' Plot difference parameter combinations versus the number of times that each
+#' was evaluated. The plot colors the results by the performanc metric that was
+#' being optimized/tested.
+#' @param x A object with class `tune_results`
+#' @return A ggplot object.
+#' @export
+plot_race <- function(x) {
+  met <- tune::.get_tune_metric_names(grid_anova)[1]
+  x %>%
+    dplyr::collect_metrics() %>%
+    dplyr::filter(.metric == met) %>%
+    dplyr::mutate(.config = reorder(.config, n)) %>%
+    ggplot2::ggplot(ggplot2::aes(x = n, y = .config)) +
+    ggplot2::geom_point(aes(col = mean)) +
+    ggplot2::geom_linerange(ggplot2::aes(xmin = 0, xmax = n), alpha = .5) +
+    ggplot2::labs(x = "Number of Resamples", y = NULL)
+}
+
