b0d93ee3fcc6caea994be9cc7b7ba6742efd1974
Max Kuhn <mxkuhn@gmail.com>
2020-11-01 17:30:16 -0500

    updated test cases

--- tests/testthat/test-sa-control.R
+++ tests/testthat/test-sa-control.R
@@ -5,7 +5,7 @@ test_that('control_sim_anneal arg passing', {
   expect_equal(control_sim_anneal(verbose = TRUE)$verbose, TRUE)
   expect_equal(control_sim_anneal(no_improve = 13)$no_improve, 13L)
   expect_equal(control_sim_anneal(restart = 2)$restart, 2)
-  expect_equal(control_sim_anneal(radius = .12)$radius, .12)
+  expect_equal(control_sim_anneal(radius = rep(.12, 2))$radius, rep(.12, 2))
   expect_equal(control_sim_anneal(flip = .122)$flip, .122)
   expect_equal(control_sim_anneal(cooling_coef = 1/10)$cooling_coef, 1/10)
   expect_equal(control_sim_anneal(extract = function(x) x)$extract, function(x) x)
@@ -69,15 +69,11 @@ test_that('control_sim_anneal bad arg passing', {
     "but the search will stop after"
   )
   expect_error(
-    control_sim_anneal(radius = 0:1),
-    "should be a single numeric value"
-  )
-  expect_error(
     control_sim_anneal(radius = "huge"),
-    "should be a single numeric value"
+    "should be two numeric values"
   )
-  expect_equal(control_sim_anneal(radius = 0)$radius, 0.001)
-  expect_equal(control_sim_anneal(radius = 2)$radius, 0.999)
+  expect_equal(control_sim_anneal(radius = c(-1, .2))$radius, c(0.001, .2))
+  expect_equal(control_sim_anneal(radius = c(15, .1))$radius, c(.1, 0.999))
   expect_error(
     control_sim_anneal(flip = 0:1),
     "should be a single numeric value"
