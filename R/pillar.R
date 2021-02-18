229a98b6db0e12d0fffc0e664e9e5f1da9fff7f1
Max Kuhn <mxkuhn@gmail.com>
2021-02-18 11:00:43 -0500

    add obj_sum and size_sum for racing objects

---
+++ R/pillar.R
@@ -0,0 +1,25 @@
+
+#' @export
+obj_sum.tune_race <- function(x) {
+  null_metrics <- purrr::map_lgl(x$.metrics, is.null)
+  if (!all(null_metrics)) {
+    res <- "race[+]"
+  } else {
+    res <- "race[x]"
+  }
+  res
+}
+
+#' Helpers for pillar formatting
+#' @param x an object
+#' @return A character string.
+#' @export
+#' @keywords internal
+#' @rdname pillar-helpers
+size_sum.tune_race <- function(x) {
+  ""
+}
+
+newer_tibble <- function() {
+  utils::packageVersion("tibble") >= "3.0.6"
+}
