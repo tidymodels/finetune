33a9fb556760cd8162584283d466c6a4ec0b2103
Max Kuhn <mxkuhn@gmail.com>
2021-06-28 19:36:16 -0400

    fix issues for #7

--- tests/testthat/test-win-loss-overall.R
+++ tests/testthat/test-win-loss-overall.R
@@ -51,3 +51,51 @@ test_that('variable interface', {
   expect_equal(class(res), c("tune_race", "tune_results", "tbl_df", "tbl", "data.frame"))
   expect_true(nrow(collect_metrics(res)) == 10)
 })
+
+# ------------------------------------------------------------------------------
+
+test_that('one player is really bad', {
+  skip_on_cran()
+  library(workflows)
+  library(rsample)
+  library(parsnip)
+  library(tibble)
+  library(ranger)
+
+  set.seed(1341)
+  df <- tibble(
+    x1 = rnorm(500, 1:500),
+    x2 = sample(c(1:4), size = 500, replace = T)
+  ) %>%
+    mutate(
+      y = rbinom(500, 1, prob = (x1 / max(x1))) %>% as.factor()
+    )
+
+  set.seed(121)
+  df_folds <- vfold_cv(df, strata = .data$y)
+
+  rf_spec <-
+    rand_forest(min_n = tune(), trees = 10) %>%
+    set_engine('ranger') %>%
+    set_mode("classification")
+
+  wf <- workflow() %>%
+    add_formula(y ~ .) %>%
+    add_model(rf_spec)
+
+  grid <- tibble(min_n = c(1, 400))
+  ctrl <- control_race(burn_in = 2, alpha = .05, randomize = TRUE)
+  expect_error({
+    set.seed(3355)
+    tuning_results <- tune_race_win_loss(
+      wf,
+      resamples = df_folds,
+      metrics = metric_set(roc_auc),
+      grid = grid,
+      control =
+    )
+  },
+  regex = NA,
+  )
+  expect_equal(nrow(show_best(tuning_results)), 1)
+})
