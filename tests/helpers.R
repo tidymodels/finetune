3c47aed203407c856457ece5ad779bf36b898793
Max Kuhn <mxkuhn@gmail.com>
2022-03-22 20:52:35 -0400

    allow checking without packages in Suggests

--- tests/helpers.R
+++ tests/helpers.R
@@ -4,52 +4,54 @@ library(workflows)
 library(parsnip)
 library(dplyr)
 library(recipes)
-library(modeldata)
 
 # ------------------------------------------------------------------------------
 
-data(cells, package = "modeldata")
-cells <- cells %>% dplyr::select(class, contains("ch_1"))
-set.seed(33)
-cell_folds <- vfold_cv(cells, v = 3, repeats = 2)
+if (rlang::is_installed("modeldata")) {
 
-## -----------------------------------------------------------------------------
+  data(cells, package = "modeldata")
+  cells <- cells %>% dplyr::select(class, contains("ch_1"))
+  set.seed(33)
+  cell_folds <- vfold_cv(cells, v = 3, repeats = 2)
 
-cart_spec <-
-  decision_tree(cost_complexity = tune(), min_n = tune()) %>%
-  set_mode("classification") %>%
-  set_engine("rpart")
+  ## -----------------------------------------------------------------------------
 
-cart_rec <-
-  recipe(class ~ ., data = cells) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), num_comp = tune())
+  cart_spec <-
+    decision_tree(cost_complexity = tune(), min_n = tune()) %>%
+    set_mode("classification") %>%
+    set_engine("rpart")
 
-## -----------------------------------------------------------------------------
+  cart_rec <-
+    recipe(class ~ ., data = cells) %>%
+    step_normalize(all_predictors()) %>%
+    step_pca(all_predictors(), num_comp = tune())
 
-rec_wflow <-
-  cell_knn <-
-  workflow() %>%
-  add_model(cart_spec) %>%
-  add_recipe(cart_rec)
+  ## -----------------------------------------------------------------------------
 
-f_wflow <-
-  cell_knn <-
-  workflow() %>%
-  add_model(cart_spec) %>%
-  add_formula(class ~ .)
+  rec_wflow <-
+    cell_knn <-
+    workflow() %>%
+    add_model(cart_spec) %>%
+    add_recipe(cart_rec)
 
-var_wflow <-
-  cell_knn <-
-  workflow() %>%
-  add_model(cart_spec) %>%
-  add_variables(class, everything())
+  f_wflow <-
+    cell_knn <-
+    workflow() %>%
+    add_model(cart_spec) %>%
+    add_formula(class ~ .)
+
+  var_wflow <-
+    cell_knn <-
+    workflow() %>%
+    add_model(cart_spec) %>%
+    add_variables(class, everything())
 
 
-# ------------------------------------------------------------------------------
+  # ------------------------------------------------------------------------------
 
-grid_mod <-
-  expand.grid(cost_complexity = c(0.001, 0.0001), min_n = c(3, 4))
+  grid_mod <-
+    expand.grid(cost_complexity = c(0.001, 0.0001), min_n = c(3, 4))
 
-grid_mod_rec <-
-  expand.grid(cost_complexity = c(0.001, 0.0001), min_n = 3:4, num_comp = 19:20)
+  grid_mod_rec <-
+    expand.grid(cost_complexity = c(0.001, 0.0001), min_n = 3:4, num_comp = 19:20)
+}
